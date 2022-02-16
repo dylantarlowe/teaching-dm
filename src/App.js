6cd5269f9ad82c447f97a1aace1b75aaa8d0a3f3
Dylan Tarlowe <dylantarlowe>
2022-02-15 21:19:43 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+        <p>LOGO</p>
+        <p>Contact Us</p>
+        <p>About</p>
+        <p>Get Involved</p>
+        <p>Donate</p>
+        <p>Store</p>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;

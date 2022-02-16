998e0e57ade4e68cd00aa472754233e9d6df754e
Dylan Tarlowe <dylantarlowe>
2022-02-15 21:28:32 -0500

    new stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import TotalRevealhome from './Images/TotalRevealhome.jpeg';
 function App() {
   return (
     <div className="App">
@@ -11,7 +11,9 @@ function App() {
         <p>Get Involved</p>
         <p>Donate</p>
         <p>Store</p>
-        
+      </div>
+      <div className="home-background-image">
+        <img className="image" src={TotalRevealhome} />
       </div>
     </div>
   );
